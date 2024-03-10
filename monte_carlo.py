19355936172cfff58a4453974bdb2fd603f3f654
salehzaidan <salehzaidan64@gmail.com>
2024-03-10 18:42:43 +0700

    Implement Monte Carlo On-policy

--- monte_carlo.py
+++ monte_carlo.py
@@ -5,7 +5,7 @@ from typing import Any
 
 from gridworld import Gridworld
 from mdp import MDP
-from policy import TabularPolicy
+from policy import TabularPolicy, TabularStochasticPolicy
 from qfunction import QTable
 
 
@@ -58,6 +58,62 @@ def monte_carlo_exploring_starts(
     return policy, qtable
 
 
+def monte_carlo_on_policy(
+    mdp: MDP,
+    *,
+    eps: float = 0.01,
+    max_iterations: int = 10_000,
+    options: dict[str, Any] | None = None,
+):
+    policy = TabularStochasticPolicy(mdp.get_actions()[0])
+    qtable = QTable()
+    returns = defaultdict(lambda: (0.0, 0.0))  # (average, count)
+    for _ in range(max_iterations):
+        episode = []
+        state, _ = mdp.reset(options=options)
+        done = False
+        while not done:
+            action = policy.pick_action(state)
+            next_state, reward, terminated, truncated, _ = mdp.step(action)
+            episode.append((state, action, reward))
+            state = next_state
+            done = terminated or truncated
+
+        g = 0
+        for i, (state, action, reward) in enumerate(reversed(episode)):
+            g = mdp.get_discount_factor() * g + reward
+            if is_first_visit(state, action, len(episode) - 1 - i, episode):
+                average, count = returns[(state, action)]
+                returns[(state, action)] = average, count = (
+                    (average * count + g) / (count + 1),
+                    count + 1,
+                )
+                qtable.set(
+                    state,
+                    action,
+                    average,
+                )
+
+                max_value = -float("inf")
+                greedy_action = action
+                actions = mdp.get_actions(state)
+                for action in actions:
+                    value = qtable.get(state, action)
+                    if value > max_value:
+                        max_value = value
+                        greedy_action = action
+
+                for action in actions:
+                    if action == greedy_action:
+                        prob = 1 - eps + eps / len(actions)
+                    else:
+                        prob = eps / len(actions)
+
+                    policy.update(state, action, prob)
+
+    return policy, qtable
+
+
 def is_first_visit(target_state, target_action, target_index, episode):
     for state, action, _ in episode[:target_index]:
         if state == target_state and action == target_action:
@@ -67,8 +123,7 @@ def is_first_visit(target_state, target_action, target_index, episode):
 
 if __name__ == "__main__":
     env = Gridworld(4, 3, trap_positions=[(3, 1)], wall_positions=[(1, 1)])
-    policy, qtable = monte_carlo_exploring_starts(
-        env, options={"randomize_position": True}
-    )
+    policy, qtable = monte_carlo_on_policy(env, max_iterations=100_000)
     print(env.visualize_policy(policy))
+    pprint.pprint(policy.table)
     pprint.pprint(qtable.table)
--- policy.py
+++ policy.py
@@ -1,3 +1,4 @@
+import random
 from collections import defaultdict
 
 
@@ -11,6 +12,14 @@ class DeterministicPolicy(Policy):
         raise NotImplementedError
 
 
+class StochasticPolicy(Policy):
+    def update(self, state, action, probability):
+        raise NotImplementedError
+
+    def get_probability(self, state, action):
+        raise NotImplementedError
+
+
 class TabularPolicy(DeterministicPolicy):
     def __init__(self, default_action):
         self.table = defaultdict(lambda: default_action)
@@ -20,3 +29,29 @@ class TabularPolicy(DeterministicPolicy):
 
     def update(self, state, action):
         self.table[state] = action
+
+
+class TabularStochasticPolicy(StochasticPolicy):
+    def __init__(self, default_action):
+        self.table = defaultdict(lambda: 0.0)
+        self.default_action = default_action
+
+    def pick_action(self, state):
+        table = [
+            (action, prob)
+            for (state_, action), prob in self.table.items()
+            if state_ == state
+        ]
+        cumulative_prob = 0.0
+        for action, prob in table:
+            cumulative_prob += prob
+            if random.random() < cumulative_prob:
+                return action
+
+        return self.default_action
+
+    def update(self, state, action, probability):
+        self.table[(state, action)] = probability
+
+    def get_probability(self, state, action):
+        return self.table[(state, action)]
